d80f81e737c7a02719f7a11b37e6d9fe5108d609
Rajeshwarikota <kotarajeswari003.com>
2023-03-03 14:41:29 +0530

    [Rajeshwari] Adding first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -11,6 +11,7 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine(".....User Registration......");
+            Validating.ValidatingFirstName();
             Console.ReadLine();
         }
     }
---
+++ UserRegistration/Validating.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Validating
+    {
+
+        public static void ValidatingFirstName()
+        {
+            Console.WriteLine("enter the fitst name");
+            string pattern = "^[A-Z]{1}[a-z]{2}";
+            string input = Console.ReadLine();
+            validatingpattern(input, pattern);
+        }
+        public static void validatingpattern(string input, string pattern)
+        {
+            if (Regex.IsMatch(input, pattern))
+            {
+                Console.WriteLine("{0} is valid ", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not valid ", input);
+            }
+
+        }
+    }
+}
