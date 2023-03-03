dec1b327904abfc9cd1cb460ee4776a7a0eda3d3
Rajeshwarikota <kotarajeswari003.com>
2023-03-03 15:03:46 +0530

    [Rajeshwari] Adding Password uppercase

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -11,11 +11,33 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine(".....User Registration......");
-            Validating.ValidatingFirstName();
-            Validating.ValidatingLastName();
-            Validating.ValidatingEmail();
-            Validating.ValidatingPhoneNumber();
-            Validating.Validatingpassword();
+
+            Validating validate = new Validating();
+            Console.WriteLine("Please Choose \n1.firstname \n2.lastname \n3.email \n4.phonenum \n5.password  \n20.exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    validate.ValidatingFirstName();
+                    break;
+                case 2:
+                    validate.ValidatingLastName();
+                    break;
+                case 3:
+                    validate.ValidatingEmail();
+                    break;
+                case 4:
+                    validate.ValidatingPhoneNumber();
+                    break;
+                case 5:
+                    validate.Validatingpassword();
+                    break;
+              
+
+                default:
+                    Console.WriteLine("Choose the Correct Number !");
+                    break;
+            }
             Console.ReadLine();
         }
     }
--- UserRegistration/Validating.cs
+++ UserRegistration/Validating.cs
@@ -10,38 +10,38 @@ namespace UserRegistration
     internal class Validating
     {
 
-        public static void ValidatingFirstName()
+        public  void ValidatingFirstName()
         {
             Console.WriteLine("enter the fitst name");
             string pattern = "^[A-Z]{1}[a-z]{2}";
             string input = Console.ReadLine();
             validatingpattern(input, pattern);
         }
-        public static void ValidatingLastName()
+        public  void ValidatingLastName()
         {
             Console.WriteLine("enter the last name");
             string pattern = "^[A-Z]{1}[a-z]{2,}";
             string input = Console.ReadLine();
             validatingpattern(input, pattern);
         }
-        public static void ValidatingEmail()
+        public void ValidatingEmail()
         {
             Console.WriteLine("enter the Email");
             string pattern = "^[a,b,c]{3}[.][A-Z]{3}[@][b,l]{2}[.][c,o]{2}[.][a-z]{2}";
             string input = Console.ReadLine();
             validatingpattern(input, pattern);
         }
-        public static void ValidatingPhoneNumber()
+        public void ValidatingPhoneNumber()
         {
             Console.WriteLine("enter the phone number");
             string pattern = "^[0-9]{2}\\s[0-9]{10}";
             string input = Console.ReadLine();
             validatingpattern(input, pattern);
         }
-        public static void Validatingpassword()
+        public void Validatingpassword()
         {
             Console.WriteLine("enter the 8 character");
-            string pattern = "^[a-zA-Z0-9]{8}";
+            string pattern = "^[A-Z]{1,}[a-z0-9]{7}";
             string input = Console.ReadLine();
             validatingpattern(input, pattern);
         }
